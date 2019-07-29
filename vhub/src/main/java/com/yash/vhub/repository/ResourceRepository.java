bacb3345b00946617223074ce95d199c3ddb37ea
Caleb Reiter <caleb@reiters.org>
2019-07-29 11:04:14 -0500

    added skill searching on Resource

--- vhub/src/main/java/com/yash/vhub/repository/ResourceRepository.java
+++ vhub/src/main/java/com/yash/vhub/repository/ResourceRepository.java
@@ -1,25 +1,22 @@
 package com.yash.vhub.repository;
 
 import java.util.List;
-import java.util.Set;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 import com.yash.vhub.domain.Resource;
 import com.yash.vhub.domain.ResourceSummary;
-import com.yash.vhub.domain.Skill;
 
 @RepositoryRestResource(excerptProjection=ResourceSummary.class)
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
-//	Set<Resource> findByNameAndSkill_Skill(String name, String skill);
-
+	@Query("SELECT r FROM Resource r INNER JOIN r.skills s WHERE s.skill = :skill AND r.name LIKE %:name%")
+	List<Resource> findBySkillAndName(@Param("skill") String skill, @Param("name") String name);
+	
+	@Query("SELECT r FROM Resource r INNER JOIN r.skills s WHERE s.skill = :skill")
+	List<Resource> findBySkill(@Param(value = "skill") String skill);
+	
 	List<Resource> findByNameContaining(String name);
-
-//	@Query("SELECT r FROM Resource r WHERE :skill member of r.skills")
-//	Set<Resource> findBySkill(@Param(value = "skill") Skill skill);
 }
